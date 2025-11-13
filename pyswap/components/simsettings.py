d81710a6902c714b949ba84e8f8e218ef954944f
markvdbrink <mark.vandenbrink@wur.nl>
2025-11-13 16:31:01 +0000

    feat: added possible variables csv and csv_tz to documentation generalsettings

--- pyswap/components/simsettings.py
+++ pyswap/components/simsettings.py
@@ -1,12 +1,5 @@
 # mypy: disable-error-code="call-overload, misc, override, type-arg"
 
-"""General settings for the simulation and settings for the Richards' equation.
-
-Classes:
-    GeneralSettings: General settings of the simulation.
-    RichardsSettings: Settings for the Richards' equation.
-"""
-
 import logging as _logging
 from datetime import date as _date
 from typing import (
@@ -73,69 +66,211 @@ class GeneralSettings(_PySWAPBaseModel, _SerializableMixin, _YAMLValidatorMixin)
     """General settings of the simulation.
 
     Attributes:
-        pathwork (str): Path to the working directory. Immutable attribute.
-        pathatm (str): Path to folder with weather files. Immutable attribute.
-        pathcrop (str): Path to folder with crop files. Immutable attribute.
-        pathdrain (str): Path to folder with drainage files. Immutable attribute.
-        swscre (Literal[0, 1, 3]): Switch, display progression of simulation
+        pathwork (str): Path to the working directory. **Immutable attribute**.
+        pathatm (str): Path to folder with weather files. **Immutable attribute**.
+        pathcrop (str): Path to folder with crop files. **Immutable attribute**.
+        pathdrain (str): Path to folder with drainage files. **Immutable attribute**.
+        swscre (Literal[0, 1, 2]): Switch, display progression of simulation
             run to screen:
-            * 0 - no display to screen
-            * 1 - display water balance components
-            * 2 - display daynumber
+
+            * 0 - Don't display to screen
+            * 1 - Display water balance components
+            * 2 - Display daynumber
+
         swerror (Literal[0, 1]): Switch for printing errors to screen:
-            * 0 - no error messages
-            * 1 - print error messages
-        tstart (str): Start date of simulation run.
-        tend (str): End date of simulation run.
-        nprintday (int): Number of output times during a day
-        swmonth (Literal[0, 1]): Switch, output each month:
-            * 0 - output each month
-            * 1 - output via period, swres or swodat
-        period (Optional[int]): Fixed output interval in days
-        swres (Optional[Literal[0, 1]]): Switch, reset output interval counter
+
+            * 0 - Don't print error messages
+            * 1 - Print error messages
+
+        tstart (str): Start date of simulation run [YYYY-MM-DD].
+        tend (str): End date of simulation run [YYYY-MM-DD].
+        nprintday (int): Number of output times during a day [1..1440, -]
+        swmonth (Literal[0, 1]): Switch for when to write output:
+
+            * 0 - Output each month
+            * 1 - Output via `period`, `swres` or `swodat`
+
+        period (Optional[int]): Fixed output interval [0..366, day]
+        swres (Optional[Literal[0, 1]]): Switch whether to reset output interval counter
             each year:
+
             * 0 - No
             * 1 - Yes
-        swodat (Optional[Literal[0, 1]]): Switch, extra output dates.
+
+        swodat (Optional[Literal[0, 1]]): Switch for extra output dates.
+
             * 0 - No extra output dates
-            * 1 - Extra output dates (outdatin)
-        outdatin (Optional[DateList]): list of specific dates.
+            * 1 - Extra output dates. **Activates** `outdatin`
+
+        outdatin (Optional[DateList]): List of specific dates [YYY-MM-DD].
         swyrvar (Literal[0, 1]): Output times for overall water and solute
             balances in *.BAL and *.BLC file: choose output at a fixed date
             each year or at different dates
-            * 0 - each year output at the same date (datefix)
-            * 1 - output at specific dates (outdat)
-        datefix (Optional[DayMonth]): Fixed date for output
-        outdat (Optional[DateList]): Specify all output dates
-        outfil (str): Generic file name of output files. Immutable attribute.
+
+            * 0 - each year output at the same date. **Activates** `datefix`
+            * 1 - output at specific dates. **Activates** `outdat`
+
+        datefix (Optional[DayMonth]): Fixed date for output [YYYY-MM-DD].
+        outdat (Optional[DateList]): Specify all output dates [YYYY-MM-DD]
+        outfil (str): Generic file name of output files. **Immutable attribute**.
         swheader (Literal[0, 1]): Print header at the start of each
             balance period:
-            * 0 - No header
+
+            * 0 - Don't print header
             * 1 - Print header
-        extensions (list): List of file extensions SWAP should return.
-            Available options are: ["wba", "end", "vap", "bal", "blc", "sba", "ate",
-            "bma", "drf", "swb", "ini", "inc", "crp", "str", "irg", "csv", "csv_tz"]
-            TODO: Add description for each extension
+
+        extensions (list): List of file extensions SWAP should return. It is recommended to use
+            "csv" and/or "csv_tz", as it includes the information of the other files.
+            Available options are:
+
+            * "csv": Variables over time
+            * "csv_tz": Variables over time and depth
+            * "wba": Cumulative water balance
+            * "end": End-conditions
+            * "vap": Soil profiles of moisture, solute and temperature
+            * "bal": Yearly water balance
+            * "blc": Detailed yearly water balance
+            * "sba": Cumulative solute balance
+            * "ate": Soil temperature profiles
+            * "bma": Water fluxes (only for macropore flow)
+            * "drf": Drainage fluxes (only for extended drainage)
+            * "swb": Surface water reservoir (only for extended drainage)
+            * "ini": Initial soil physical and heat parameters
+            * "inc": Water balance increments
+            * "crp": Simple or detailed crop growth model output
+            * "str": Oxygen, drought, salinity and frost stress
+            * "irg": Irrigation gifts
+
         inlist_csv (Optional[StringList]): List of variables for the csv output.
-            Available options are: TODO
+            Available options for the total water balance are:
+
+            * "RAIN" - Rainfall for current time interval [cm]
+            * "SNOW" - Snowfall for current time interval [cm]
+            * "IRRIG" - Irrigation for current time interval [cm]
+            * "INTERC" - Crop interception for current time interval [cm]
+            * "RUNON" - Runon for current time interval [cm]
+            * "RUNOFF" - Runoff for current time interval [cm]
+            * "TACT" - Crop transpiration for current time interval [cm]
+            * "EACT" - Soil evaporation for current time interval [cm]
+            * "SUBLIM" - Sublimated snow [cm]
+            * "DRAINAGE" - Total drainage for current time interval [cm]
+            * "VOLACT" - Water storage (L) of soil column [cm]
+            * "DSTOR" - Change in water storage of [cm]
+            * "QBOTTOM" - Net flow across bottom boundary for current time interval [cm]
+
+            Miscellaneous variables with respect to ponding and groundwater level are:
+
+            * "RAIN_NET" - Net rainfall for current time interval [cm]
+            * "IRRIG_NET" - Net irrigation for current time interval [cm]
+            * "SSNOW" - Amount of snow [cm]
+            * "POND" - Ponding height [cm]
+            * "GWL" - Groundwater level [cm + sl]
+
+            Variables with respect to the simulation of evapotranspiration are:
+
+            * "TPOT" - Potential crop transpiration for current time interval [cm]
+            * "TACT" - Crop transpiration for current time interval [cm]
+            * "EPOT" - Potential Soil evaporation for current time interval [cm]
+            * "EACT" - Soil evaporation for current time interval [cm]
+            * "INTERC" - Crop interception for current time interval [cm]
+            * "TREDDRY" - Reduction due to drought stress [cm]
+            * "TREDWET" - Reduction due to oxygen stress (too wet) [cm]
+            * "TREDSOL" - Reduction due to salinity stress [cm]
+            * "TREDFRS" - Reduction due to frost stress [cm]
+
+            General crop parameters are:
+
+            * "TSUM" - Temperature sum from start to end of growing season of the crop [°C]
+            * "DVS" - Crop development stage [-]
+            * "HEIGHT" - Crop height [cm]
+            * "CRPFAC" - Crop factor [-]
+            * "LAIPOT" - Leaf area index for potential run [m² m⁻²]
+            * "LAI" - Leaf area index [m² m⁻²]
+            * "RDPOT" - Rooting depth for potential run [cm]
+            * "RD" - Rooting depth [cm]
+
+            Available WOFOST variables are:
+
+            * "PGASSPOT" - Assimilation rate after nitrogen stress and max. yield, potential crop growth [kg ha⁻¹]
+            * "PGASS" - Assimilation rate after nitrogen stress and max. yield, actual crop growth [kg ha⁻¹]
+            * "CPWDM" - Dry weight of dead and living plant organs for potential growth [kg ha⁻¹]
+            * "CWDM" - Dry weight of dead and living plant organs [kg ha⁻¹]
+            * "CPWSO" - Dry weight of storage organ for potential growth [kg ha⁻¹]
+            * "CWSO" - Dry weight of storage organ [kg ha⁻¹]
+            * "PWLV" - Dry weight of plant leaves for potential growth [kg ha⁻¹]
+            * "WLV" - Dry weight of plant leaves [kg ha⁻¹]
+            * "PWST" - Dry weight of plant stem for potential growth [kg ha⁻¹]
+            * "WST" - Dry weight of plant stem [kg ha⁻¹]
+            * "PWRT" - Dry weight of plant root for potential growth [kg ha⁻¹]
+            * "WRT" - Dry weight of plant root [kg ha⁻¹]
+            * "DWSO" - Dry weight of plant storage organs of actual crop [kg ha⁻¹]
+            * "DWLV" - Dry weight of plant leaves of actual crop [kg ha⁻¹]
+            * "DWLVPOT" - Dry weight of plant leaves of potential crop [kg ha⁻¹]
+            * "DWST" - Dry weight of plant stem of actual crop [kg ha⁻¹]
+            * "DWSTPOT" - Dry weight of plant stem of potential crop [kg ha⁻¹]
+            * "DWRT" - Dry weight of plant roots of actual crop [kg ha⁻¹]
+            * "DWRTPOT" - Dry weight of plant roots of potential crop [kg ha⁻¹]
+            * "PGRASSDM" - Dry weight of dead and living grass organs for potential run [kg ha⁻¹]
+
+            Variables for the grass module of WOFOST are:
+
+            * "GRASSDM" - Dry weight of dead and living grass organs [kg ha⁻¹]
+            * "PMOWDM" - Dry weight of harvested grass for potential run [kg ha⁻¹]
+            * "MOWDM" - Dry weight of harvested grass [kg ha⁻¹]
+            * "PGRAZDM" - Cumulative dry weight of grass consumed with grazing for potential run [kg ha⁻¹]
+            * "GRAZDM" - Cumulative dry weight of grass consumed with grazing [kg ha⁻¹]
+            * "PLOSSDM" - Total loss of potential harvest due to insufficient pressure head [kg ha⁻¹]
+            * "LOSSDM" - Total loss of actual harvest due to insufficient pressure head [kg ha⁻¹]
+
+            Variables that involve solute transport are:
+
+            * "SQPREC" - Cumulative amount of solutes in precipitation [g cm⁻²]
+            * "SQIRRIG" - Cumulative amount of solutes in irrigation water [g cm⁻²]
+            * "SQBOT" - Cumulative amount of solutes passed through the soil column bottom [g cm⁻²]
+            * "SQDRA" - Total amount of solutes transported to drainage canals [g cm⁻²]
+            * "DECTOT" - Cumulative amount of solute decomposition [g cm⁻²]
+            * "ROTTOT" - Cumulative amount of solutes extracted by plant roots [g cm⁻²]
+            * "SAMPRO" - Total amount of solutes in soil column [g cm⁻²]
+            * "SOLBAL" - Cumulative solute balance (M/L²) for current balance period [g cm⁻²]
+
         inlist_csv_tz (Optional[StringList]): List of variables over depth for
             the csv tz output. Available options are:
-            TODO
+
+            * "H" - Pressure head [cm]
+            * "WC" - Volumetric water content [cm³ cm⁻³]
+            * "TEMP" - Soil temperature [°C]
+            * "K" - Hydraulic conductivity [cm d⁻¹]
+            * "CONC" - Solute concentration [g cm⁻³]
+            * "CONCADS" - Adsorbed solute content [g cm⁻³]
+            * "O2TOP" - Oxygen concentration at top of compartment [kg m⁻³]
+            * "HEACAP" - Soil heat capacity [J m⁻³ K⁻¹]
+            * "HEACON" - Soil heat conductivity [W m⁻¹ K⁻¹]
+            * "RWU" - Root water uptake [cm d⁻¹]
+            * "PRWU" - Potential root water uptake [cm d⁻¹]
+            * "RDENS" - Relative root distribution [-]
+            * "LRV" - Root length density distribution [cm cm]
+
         swafo (Literal[0, 1, 2]): Switch, output file with formatted hydrological data:
-            * 0 - no output
-            * 1 - output to a file named *.AFO
-            * 2 - output to a file named *.BFO
+
+            * 0 - No output.
+            * 1 - Output to a file named .AFO. **Activates** `critdevmasbal`, `swdiscrvert`
+            * 2 - Output to a file named .BFO. **Activates** `critdevmasbal`, `swdiscrvert`
+
         swaun (Literal[0, 1, 2]): Switch, output file with unformatted hydrological data:
-            * 0 - no output
-            * 1 - output to a file named *.AUN
-            * 2 - output to a file named *.BUN
-        critdevmasbal (Optional[float]): Critical Deviation in
-            water balance during PERIOD [0.0 .. 1.0 cm]
+
+            * 0 - No output
+            * 1 - Output to a file named .AUN. **Activates** `critdevmasbal`, `swdiscrvert`
+            * 2 - Output to a file named .BUN. **Activates** `critdevmasbal`, `swdiscrvert`
+
+        critdevmasbal (Optional[float]): Critical deviation in
+            water balance during `period` [0.0..1.0, cm].
         swdiscrvert (Literal[0, 1]): Switch to convert vertical discretization:
-            * 0 - no conversion
-            * 1 - convert vertical discretization
-        numnodnew (Optional[int]): New number of nodes.
-        dznew (Optional[FloatList]): New thickness of compartments.
+
+            * 0 - No conversion
+            * 1 - Convert vertical discretization. **Activates** `numnodnew`, `dznew`.
+
+        numnodnew (Optional[int]): New number of nodes [1..macp, -].
+        dznew (Optional[FloatList]): New thickness of compartments [1e-6..5e2, cm].
     """
 
     model_config = _ConfigDict(
@@ -144,17 +279,14 @@ class GeneralSettings(_PySWAPBaseModel, _SerializableMixin, _YAMLValidatorMixin)
     _all_extensions: _ClassVar[list[str]] = _EXTENSIONS
     extensions: list[str] = _Field(default_factory=list, exclude=True)
     exts: _Subsection[_ExtensionMixin] | None = None
-
     pathwork: _String = _Field(default=_BASE_PATH, frozen=True)
     pathatm: _String = _Field(default=_BASE_PATH, frozen=True)
     pathcrop: _String = _Field(default=_BASE_PATH, frozen=True)
     pathdrain: _String = _Field(default=_BASE_PATH, frozen=True)
     swscre: _Literal[0, 1, 3] = 0
     swerror: _Literal[0, 1] = 0
-
     tstart: _date | None = None
     tend: _date | None = None
-
     nprintday: int = _Field(default=1, ge=1, le=1440)
     swmonth: _Literal[0, 1] | None = None  # 1
     swyrvar: _Literal[0, 1] | None = None  # 0
@@ -164,10 +296,8 @@ class GeneralSettings(_PySWAPBaseModel, _SerializableMixin, _YAMLValidatorMixin)
     outdatin: _Arrays | None = None
     datefix: _DayMonth | None = None
     outdat: _Arrays | None = None
-
     outfil: _String = _Field(default=_FNAME_OUT, frozen=True)
     swheader: _Literal[0, 1] = 0
-
     inlist_csv: _StringList | None = None
     inlist_csv_tz: _StringList | None = None
     swafo: _Literal[0, 1, 2] = 0
@@ -202,23 +332,27 @@ class RichardsSettings(_PySWAPBaseModel, _SerializableMixin, _YAMLValidatorMixin
 
     Attributes:
         swkmean (Literal[1, 2, 3, 4, 5, 6]): Switch for averaging method of hydraulic conductivity:
-            * 1 - unweighted arithmic mean
-            * 2 - weighted arithmic mean
-            * 3 - unweighted geometric mean
-            * 4 - weighted geometric mean
-            * 5 - unweighted harmonic mean
-            * 6 - weighted harmonic mean
+
+            * 1 - Unweighted arithmic mean (recommended)
+            * 2 - Weighted arithmic mean
+            * 3 - Unweighted geometric mean
+            * 4 - Weighted geometric mean
+            * 5 - Unweighted harmonic mean
+            * 6 - Weighted harmonic mean
+
         swkimpl (Literal[0, 1]): Switch for updating hydraulic conductivity during iteration:
-            * 0 - no update
-            * 1 - update
-        dtmin (float): Minimum timestep [1.0e-7 .. 0.1 d]
-        dtmax (float): Maximum timestep [dtmin .. 1 d]
-        gwlconv (float): Maximum difference of groundwater level between time steps [1.0e-5 .. 1.0e3 cm]
-        critdevh1cp (float): Maximum relative difference in pressure heads per compartment [1.0e-10 .. 1.0e3]
-        critdevh2cp (float): Maximum absolute difference in pressure heads per compartment [1.0e-10 .. 1.0e3 cm]
-        critdevponddt (float): Maximum water balance error of ponding layer [1.0e-6 .. 0.1 cm]
-        maxit (int): Maximum number of iteration cycles [5 .. 100]
-        maxbacktr (int): Maximum number of back track cycles within an iteration cycle [1 .. 10]
+
+            * 0 - Don't update
+            * 1 - Update
+
+        dtmin (float): Minimum timestep [1.0e-7..0.1, d]
+        dtmax (float): Maximum timestep [dtmin..1, d]
+        gwlconv (float): Maximum difference of groundwater level between time steps [1.0e-5..1.0e3, cm]
+        critdevh1cp (float): Maximum relative difference in pressure heads per compartment [1.0e-10..1.0e3, cm]
+        critdevh2cp (float): Maximum absolute difference in pressure heads per compartment [1.0e-10..1.0e3, cm]
+        critdevponddt (float): Maximum water balance error of ponding layer [1.0e-6..0.1, cm]
+        maxit (int): Maximum number of iteration cycles [5..100, -]
+        maxbacktr (int): Maximum number of back track cycles within an iteration cycle [1..10, -]
     """
 
     swkmean: _Literal[1, 2, 3, 4, 5, 6] | None = None
